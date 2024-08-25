3f03165618ee4601cd170fd2a4d76db28c8005d0
koovalin <34717587+koovalin@users.noreply.github.com>
2024-08-25 19:04:13 +0300

    Update module_5_3.py

--- module_5_3.py
+++ module_5_3.py
@@ -15,32 +15,44 @@ class House:
 
     def __len__(self):
         return self.number_of_floors
-    
+
     def __eq__(self, other):
-        return self.number_of_floors == other.number_of_floors
-    
+        if isinstance(other, House):
+            return self.number_of_floors == other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors == other
+
     def __lt__(self, other):
-        return self.number_of_floors < other.number_of_floors
-    
+        if isinstance(other, House):
+            return self.number_of_floors < other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors < other
+
     def __le__(self, other):
-        return self.number_of_floors <= other.number_of_floors
-    
+        return self.__eq__(other) or self.__lt__(other)
+
     def __gt__(self, other):
-        return self.number_of_floors > other.number_of_floors
-    
+        return not self.__le__(other)
+
     def __ge__(self, other):
-        return self.number_of_floors >= other.number_of_floors
-    
+        return not self.__lt__(other)
+
     def __ne__(self, other):
-        return self.number_of_floors != other.number_of_floors
-    
+        if isinstance(other, House):
+            return self.number_of_floors != other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors != other
+
     def __add__(self, value):
-        self.number_of_floors += value
+        if isinstance(value, int):
+            self.number_of_floors += value
+        elif isinstance(value, House):
+            self.number_of_floors += value.number_of_floors
         return self
-    
+
     def __iadd__(self, value):
         return self.__add__(value)
-    
+
     def __radd__(self, value):
         return self.__add__(value)
 
@@ -49,7 +61,6 @@ h1 = House('ЖК Горский', 18)
 h2 = House('Домик в деревне', 2)
 h1.go_to(5)
 h2.go_to(10)
-
 
 h1 = House('ЖК Эльбрус', 10)
 h2 = House('ЖК Акация', 20)
@@ -62,21 +73,25 @@ print(h2)
 print(len(h1))
 print(len(h2))
 
-print(h1 == h2) # __eq__
+print(h1 == h2)  # __eq__
+print("----------")
+print(h1 == 10)  # __eq__
 
-
-h1 = h1 + 10 # __add__
+h1 = h1 + 10  # __add__
 print(h1)
 print(h1 == h2)
 
-h1 += 10 # __iadd__
+h1 += 10  # __iadd__
 print(h1)
 
-h2 = 10 + h2 # __radd__
+h2 = 10 + h2  # __radd__
 print(h2)
 
-print(h1 > h2) # __gt__
-print(h1 >= h2) # __ge__
-print(h1 < h2) # __lt__
-print(h1 <= h2) # __le__
-print(h1 != h2) # __ne__
+print(h1 > h2)  # __gt__
+print(h1 >= h2)  # __ge__
+print(h1 < h2)  # __lt__
+print("--------")
+print(h1 < 10)  # __lt__
+
+print(h1 <= h2)  # __le__
+print(h1 != h2)  # __ne__
