ee1a0af3863f81e2709417d12b41239a6816165d
koovalin <koovalin@gmail.com>
2024-12-09 21:36:41 +0300

    updated

--- module_13/module_13_3.py
+++ module_13/module_13_3.py
@@ -1,7 +1,7 @@
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
-API = '7757947891:AAHAK5FKxIL4mdW-SMN-cxEHefs78K0l2lk'
+API = 'YOUR_BOT_TOKEN '
 bot = Bot(token=API)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
@@ -9,11 +9,13 @@ dp = Dispatcher(bot, storage=MemoryStorage())
 @dp.message_handler(commands=['start'])
 async def start_command(message: types.Message):
     print('Привет! Я бот, помогающий твоему здоровью.')
+    await message.reply("Привет! Я бот помогающий твоему здоровью.")
 
 
 @dp.message_handler()
 async def all_messages(message: types.Message):
     print('Введите команду /start, чтобы начать общение.')
+    await message.reply('Введите команду /start, чтобы начать общение.')
 
 
 if __name__ == "__main__":
